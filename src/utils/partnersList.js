18e0ed35b0ef8c5f6a7b17d89890c001fe05b2f5
Beka <roadtothedreambeka81@gmail.com>
2022-12-13 01:05:55 +0600

    changed partners logo

--- src/utils/partnersList.js
+++ src/utils/partnersList.js
@@ -6,13 +6,11 @@ import partnerLogo5 from "../assets/images/partnersLogo5.png"
 import partnerLogo6 from "../assets/images/partnersLogo6.png"
 import partnerLogo7 from "../assets/images/partnersLogo7.png"
 import partnerLogo8 from "../assets/images/partnersLogo8.png"
-import partnerLogo9 from "../assets/images/partnersLogo9.png"
 import partnerLogo10 from "../assets/images/partnersLogo10.png"
 import partnerLogo11 from "../assets/images/partnersLogo11.png"
 import partnerLogo12 from "../assets/images/partnersLogo12.png"
 import partnerLogo13 from "../assets/images/partnersLogo13.png"
 import partnerLogo14 from "../assets/images/partnersLogo14.png"
-import partnerLogo15 from "../assets/images/partnersLogo15.png"
 import partnerLogo16 from "../assets/images/partnersLogo16.png"
 import partnerLogo17 from "../assets/images/partnersLogo17.png"
 
@@ -51,10 +49,6 @@ export const partnersList = [
     image: partnerLogo8
   },
   {
-    id: 8,
-    image: partnerLogo9
-  },
-  {
     id: 9,
     image: partnerLogo10
   },
@@ -73,10 +67,6 @@ export const partnersList = [
   {
     id: 13,
     image: partnerLogo14
-  },
-  {
-    id: 14,
-    image: partnerLogo15
   },
   {
     id: 15,
